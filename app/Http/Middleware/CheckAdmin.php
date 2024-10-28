4a80fc483185561e86e37ac67fba79c3d5c0ddc0
czaesar <unsterdam@gmail.com>
2024-10-28 11:59:37 +0300

    Вынес проверки ролей в миддлвэйр, изменен env.example, реализация абстракций для CRUD

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+        if(!$user->hasAnyRole(['super-admin', 'admin'])) {
+            return \response()->json(['error' => 'Access denied'], Response::HTTP_FORBIDDEN);
+        }
+        return $next($request);
+    }
+}
